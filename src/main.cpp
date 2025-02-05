bbefcb29482cb2438f9d038dd1a331b27b2d5a56
JudeTulel <judeyego202@gmail.com>
2025-02-05 11:49:13 +0300

    mmh

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,66 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       jtulel                                                    */
-/*    Created:      2/5/2025, 8:31:52 AM                                      */
-/*    Description:  IQ2 project                                               */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
 #include "vex.h"
-
 using namespace vex;
 
-// A global instance of vex::brain used for printing to the IQ2 brain screen
-vex::brain       Brain;
+brain Brain;
+motor leftMotor = motor(PORT1, false);
+motor rightMotor = motor(PORT2, false);
+optical opticalSensor = optical(PORT3);
 
-// define your global instances of motors and other devices here
-vex::
+void searchForLine() {
+    // First attempt: turn right
+    leftMotor.spin(forward, 15, percent);
+    rightMotor.spin(reverse, 15, percent);
+    
+    int startTime = Brain.Timer.time(msec);
+    while(Brain.Timer.time(msec) - startTime < 1500) { // ~45-degree turn
+        if(opticalSensor.hue() < 10) {  // Found the line again
+            leftMotor.stop();
+            rightMotor.stop();
+            return;
+        }
+        this_thread::sleep_for(10);
+    }
+    
+    // If no line found, try turning left
+    leftMotor.spin(reverse, 15, percent);
+    rightMotor.spin(forward, 15, percent);
+    
+    startTime = Brain.Timer.time(msec);
+    while(Brain.Timer.time(msec) - startTime < 3000) { // ~90-degree turn
+        if(opticalSensor.hue() < 10) {  
+            // Found the line again
+            leftMotor.stop();
+            rightMotor.stop();
+            return;
+        }
+        this_thread::sleep_for(10);
+    }
+
+    // Stop motors if no line found (fail-safe)
+    leftMotor.stop();
+    rightMotor.stop();
+}
+
 
 int main() {
-	
-    Brain.Screen.printAt( 2, 30, "Hello IQ2" );
-   
+    Brain.Screen.printAt(2, 30, "Optical Line Following");
+    opticalSensor.setLight(ledState::on);
+    
     while(1) {
+        int hueValue = opticalSensor.hue();
         
-        // Allow other tasks to run
+        if (hueValue < 10) {
+            leftMotor.spin(forward, 50, percent);
+            rightMotor.spin(forward, 50, percent);
+        } else {
+            leftMotor.stop();
+            rightMotor.stop();
+            searchForLine();
+        }
+        
+        Brain.Screen.clearLine(2);
+        Brain.Screen.printAt(2, 60, "Hue: %d", hueValue);
+        
         this_thread::sleep_for(10);
     }
 }
